c4f841aae0db894fe0aa3e7ce8eb3f3666f42143
Amit⚡ <amit@mvrak.com>
2025-02-16 02:04:33 -0800

    Add files via upload

---
+++ contentScript.js
@@ -0,0 +1,24 @@
+let keysArray;
+let urlMap;
+
+(async () => {
+    const url = chrome.runtime.getURL('list.json');
+    try {
+      const response = await fetch(url);
+      urlMap = await response.json();
+      keysArray = Object.keys(urlMap);
+      console.log(urlMap);
+      main();
+    } catch (error) {
+      console.error('Error loading JSON:', error);
+    }
+  })();
+
+function main(){
+    const observer=new MutationObserver(e=>{e.forEach(e=>{"childList"===e.type&&e.addedNodes.forEach(e=>{e.nodeType===Node.ELEMENT_NODE&&e.querySelectorAll('[data-testid="User-Name"]').forEach(e=>{processUserName(e)})})})});
+    function processUserName(e){username=e.children[1].firstChild.firstChild.firstChild.firstChild.firstChild.textContent,console.log(username),shouldAddIcon(username,keysArray)&&(e.hasAttribute("usaidsprocessed")||(iconnode=e.firstChild.firstChild.firstChild.firstChild.children[1],usaidsnode=iconnode.firstChild.cloneNode(),usaidsnode.appendChild(document.createElement("img")),usaidsnode.firstChild.src=chrome.runtime.getURL("usaids.png"),usaidsnode.firstChild.style="max-width:20px;max-height:20px;",iconnode.appendChild(usaidsnode),refnode=e.children[1].firstChild.children[2].cloneNode(!0),refnode.firstChild.href=getCustomURL(username,urlMap),refnode.firstChild.target="_blank",refnode.firstChild.firstChild.remove(),refnode.firstChild.textContent="US Aids Reference",refnode.firstChild.style.marginLeft="8px",refnode.firstChild.setAttribute("aria-label","US Aids Reference Link"),e.children[1].firstChild.children[2].appendChild(refnode),e.setAttribute("usaidsprocessed","true")))}
+    observer.observe(document.body,{childList:true,subtree:true});
+}
+
+function shouldAddIcon(userName,keysArray) {return keysArray.includes(userName);}
+function getCustomURL(userName, urlMap) {return urlMap[userName] || "#";}
